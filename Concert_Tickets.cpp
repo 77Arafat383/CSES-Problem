4a6f763632731d00fd2e158f01a734508cbb4c77
77Arafat383 <arafat0117@student.nstu.edu.bd>
2024-10-04 07:44:33 +0600

    Sorting and Searching

---
+++ Concert_Tickets.cpp
@@ -0,0 +1,58 @@
+//**************BISMILLAHIR RAHMANIR RAHIM****************
+#include<bits/stdc++.h>
+using namespace std;
+#define check cout<<'?'<<endl;
+#define    ll               long long
+const int M =  1e9+7;
+#define    all(x)           x.begin(), x.end()
+#define    w(x)             while(x--)
+#define    pi               acos(-1.00)
+#define rev(v) reverse(all(v));
+#define printv(a)  for(auto it:a) cout<<it<<' '; cout<<endl
+#define printm(a)  for(auto it:a) cout<<it.first<<' '<<it.second<<endl
+const int N=1e5;
+
+
+
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    multiset<int>s;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+    for(int i=0; i<m; i++)
+    {
+        int x;
+        cin>>x;
+        auto it=s.upper_bound(x);
+        if(it==s.begin())
+        {
+            cout<<-1<<endl;
+            continue;
+        }
+        it--;
+        cout<<*it<<endl;
+        s.erase(it);
+        
+    }
+}
+
+
+signed main()
+{
+ ios_base::sync_with_stdio(false) , cin.tie(NULL);
+int t;
+t=1;
+for(int i=1; i<=t; i++) 
+{
+//cout<<'t'<<i<<endl;
+solve();
+}
+    return 0;
+}
