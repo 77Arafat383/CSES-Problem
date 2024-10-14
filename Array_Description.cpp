669e1f2ddd0b43777f8a6c5e9b20f84d433e9f82
77Arafat383 <arafat0117@student.nstu.edu.bd>
2024-10-14 11:06:33 +0600

    DP

--- Array_Description.cpp
+++ Array_Description.cpp
@@ -12,19 +12,57 @@ const int M =  1e9+7;
 #define printm(a)  for(auto it:a) cout<<it.first<<' '<<it.second<<endl
 const int N=1e5;
 
-int f(int node,int prev, vector<int>&v)
-{
-    if(node<0) return 1;
-}
+
 
 
 void solve()
 {
     int n,m;
     cin>>n>>m;
-    vector<int>v(n);
+    vector<ll>v(n);
     for(auto &it:v) cin>>it;
+    vector<vector<ll>>dp(n+1,vector<ll>(m+1,0));
+    if(v[0]!=0) dp[0][v[0]-1]=1;
+    else
+    {
+        for(int i=0; i<m; i++) dp[0][i]=1;
+    }
+    for(int i=1; i<n; i++)
+    {
+        if(v[i]!=0)
+        {
+            if(v[i]-1!=0) dp[i][v[i]-1] =(dp[i][v[i]-1]+dp[i-1][v[i]-2])%M;
+            dp[i][v[i]-1]=(dp[i][v[i]-1]+dp[i-1][v[i]-1])%M;
+            if(v[i]-1!=m-1) dp[i][v[i]-1]=(dp[i][v[i]-1]+dp[i-1][v[i]])%M;
+        }
+        else
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(j!=0) dp[i][j]=(dp[i][j]+dp[i-1][j-1])%M;
+            dp[i][j]=(dp[i][j]+dp[i-1][j])%M;
+            if(j!=m-1) dp[i][j]=(dp[i][j]+dp[i-1][j+1])%M;
 
+            }
+        }
+    }
+//     for(int i=0; i<=n; i++)
+//    {
+//     for(int j=0; j<=m; j++) cout<<dp[i][j]<<" "; cout<<endl;
+//    }
+    if(v[n-1]!=0)
+    {
+        cout<<dp[n-1][v[n-1]-1]<<endl;
+        return;
+    }
+    ll sum=0;
+    for(int i=0; i<=m; i++)
+    {
+        sum=(sum+dp[n-1][i])%M;
+    }
+    cout<<sum<<endl;
+    
+   
 }
 
 
